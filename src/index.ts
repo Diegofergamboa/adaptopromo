723f9bd6e31d8ddbfd653b2ede90fc275ad130c3
diegofergamboa <diegofergamboa@hotmail.es>
2023-12-15 16:10:31 -0500

    Second commmit

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,13 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
-const PORT = 3000
-
+const PORT = 3000;
 
 app.get('/ping', (_req, res) => {
-    console.log('Some pinged')
-    res.send('pong'))
-})
-
+	console.log('Some pinged');
+	res.send('pong');
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+	console.log(`Server running on port ${PORT}`);
+});
